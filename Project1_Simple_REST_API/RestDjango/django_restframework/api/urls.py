10fd0c0f2ead618dfebb753ffd48e4a1dcac88b4
sabuhish <sabuhi.shukurov@gmail.com>
2020-05-11 21:53:16 +0400

    simple RestAPI with JWT && TODO application

--- Project1_Simple_REST_API/RestDjango/django_restframework/api/urls.py
+++ Project1_Simple_REST_API/RestDjango/django_restframework/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import  path
-from .views import  *
+from .views import  UserDetail,UserList,CreateUser, Todos, TodoOperations,Login
+from rest_framework_simplejwt.views import token_refresh
 
 
 urlpatterns = [
@@ -7,5 +8,11 @@ urlpatterns = [
     path('users/<int:pk>/',UserDetail.as_view()),
     path('users/',UserList.as_view()),
     path('user/',CreateUser.as_view()),
+
+    path("user/todo/",Todos.as_view()),#POST ,#GET all todos
+    path("user/todo/<int:pk>/",TodoOperations.as_view()), #PUT,GET,DELETE specifc
+
+    path("login/", Login.as_view()),
+    path("token-refresh/", token_refresh)
 
 ]
--- Project1_Simple_REST_API/RestDjango/django_restframework/api/utils.py
+++ Project1_Simple_REST_API/RestDjango/django_restframework/api/utils.py
@@ -1,5 +1,7 @@
 import logging, logging.config
 import sys
+import  jwt
+from django.conf import settings
 
 LOGGING = {
     'version': 1,
@@ -17,4 +19,18 @@ LOGGING = {
 
 logging.config.dictConfig(LOGGING)
 
+
+
+
+def jwt_decode_handler(token):
+   
+    secret_key = settings.SECRET_KEY
+    
+    return jwt.decode(
+        token,
+        secret_key,
+        audience=settings.SIMPLE_JWT.get("AUDIENCE"),
+        issuer=settings.SIMPLE_JWT.get("ISSUER"),
+        algorithms=[settings.SIMPLE_JWT.get("ALGORITHM")]
+    )
 

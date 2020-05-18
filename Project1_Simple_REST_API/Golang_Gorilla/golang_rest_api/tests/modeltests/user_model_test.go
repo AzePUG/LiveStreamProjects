7824966f592a295319ce0584bc5edbaa2805b85e
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-05-18 13:27:27 +0400

    Added failed test

--- Project1_Simple_REST_API/Golang_Gorilla/golang_rest_api/tests/modeltests/user_model_test.go
+++ Project1_Simple_REST_API/Golang_Gorilla/golang_rest_api/tests/modeltests/user_model_test.go
@@ -5,6 +5,12 @@ import (
 )
 
 func TestFindSingleUser(t *testing.T) {
+	// TODO: failing test
 	refreshUserTable()
 	seedOneUser()
+	user, _ := services.GetUsers()
+	length := len(user)
+	if length !=1 {
+		t.Errorf("The length of user slice should be 1 but got %d", length)
+	}
 }

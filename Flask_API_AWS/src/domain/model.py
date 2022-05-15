41bd7a38b3dd4bac7b088b16299fa991a0339ff5
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-05-15 14:22:23 +0400

    introducing the jpeg and pdf allocation

--- Flask_API_AWS/src/domain/model.py
+++ Flask_API_AWS/src/domain/model.py
@@ -11,3 +11,11 @@ class JPG:
 class PDF:
     dest_path: str
     extension: str = field(init=False, default=".pdf")
+
+
+def allocate_jpeg(src_path: str) -> JPG:
+    return JPG(src_path=src_path)
+
+
+def allocate_pdf(dest_path: str) -> PDF:
+    return PDF(dest_path=dest_path)
--- Flask_API_AWS/test/test_models.py
+++ Flask_API_AWS/test/test_models.py
@@ -12,3 +12,24 @@ def test_if_jpegs_are_equal():
     jpg2 = model.JPG(src_path="fake.jpg")
 
     assert jpg1 == jpg2
+
+
+def test_if_can_create_pdf():
+    pdf = model.PDF(dest_path="fake.pdf")
+    assert pdf.extension == ".pdf"
+
+
+def test_if_pdfs_are_equal():
+    pdf1 = model.PDF(dest_path="fake.pdf")
+    pdf2 = model.PDF(dest_path="fake.pdf")
+    assert pdf1 == pdf2
+
+
+def test_if_can_allocate_jpeg():
+    jpg = model.JPG(src_path="fake.jpg")
+    assert model.allocate_jpeg(src_path="fake.jpg") == jpg
+
+
+def test_if_can_allocate_pdf():
+    pdf = model.PDF(dest_path="fake.pdf")
+    assert model.allocate_pdf(dest_path="fake.pdf") == pdf

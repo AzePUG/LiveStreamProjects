4ead7ba684021ec5751d8d91cffe0e242467d113
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-05-28 16:26:32 +0400

    Adding failed rest API tests with ResponseSuccess and ResponseFailure

--- Flask_API_AWS/test/entrypoints/test_rest.py
+++ Flask_API_AWS/test/entrypoints/test_rest.py
@@ -1,11 +1,13 @@
 import datetime
 import json
 import uuid
+import pytest
 from unittest import mock
 
 from src.domain import model
 from src.domain.model import Converted
 from src.serializers.model import ConvertedJsonEncoder
+from src.responses.responses import ResponseFailure, ResponseSuccess, ResponseTypes
 
 
 def _get_converted(timestamp_: datetime.datetime, uuid_: uuid.UUID):
@@ -18,14 +20,34 @@ def _get_converted(timestamp_: datetime.datetime, uuid_: uuid.UUID):
     }
 
 
+def _converted_list(get_uuid: uuid.UUID) -> list[Converted]:
+    timestamp_ = datetime.datetime.now()
+    return [Converted.from_dict(_get_converted(timestamp_, get_uuid))]
+
+
 @mock.patch("src.entrypoints.api.rest.convert.converted_list_use_case")
 def test_get(mock_user_case, client, get_uuid):
-    timestamp_ = datetime.datetime.now()
-    converted_list = [Converted.from_dict(_get_converted(timestamp_, get_uuid))]
-    mock_user_case.return_value = converted_list
+    converted_list = _converted_list(get_uuid)
+    mock_user_case.return_value = ResponseSuccess(converted_list)
     http_response = client.get("/converteds")
     assert json.loads(http_response.data.decode("UTF-8")) == json.loads(
         json.dumps(converted_list, cls=ConvertedJsonEncoder))
     mock_user_case.assert_called()
     assert http_response.status_code == 200
     assert http_response.mimetype == "application/json"
+
+
+@pytest.mark.parametrize(
+    "response_type, expected_status_code",
+    [
+        (ResponseTypes.PARAMETERS_ERROR, 400),
+        (ResponseTypes.RESOURCE_ERROR, 404),
+        (ResponseTypes.SYSTEM_ERROR, 500),
+    ]
+)
+@mock.patch("src.entrypoints.api.rest.convert.converted_list_use_case")
+def test_get_response_failures(mock_use_case, client, response_type, expected_status_code):
+    mock_use_case.return_value = ResponseFailure(response_type, message="Just an error message")
+    http_response = client.get("/converteds")
+    mock_use_case.assert_called()
+    assert http_response.status_code == expected_status_code
